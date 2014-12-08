a0a6ea911cc70d9b1a3fba931d4cf2bb11a2ab1c
Jolita Pabludo <jolita.grazyte@student.kdg.be>
2014-12-08 14:24:30 +0100

    crud cms updated

---
+++ 032_CRUD-CMS/login-confirm.php
@@ -0,0 +1,53 @@
+<?php 
+
+
+session_start();
+
+function __autoload( $classname ) {
+    include_once ('./classes/'. $classname .'.php');
+}
+
+$isEmailValid = filter_var( $_POST['email'], FILTER_VALIDATE_EMAIL ) ? true : false;
+
+if ( isset($_POST['submit']) ) {
+
+	$email = $_POST['email'];
+	$pass  = $_POST['password'];
+
+	$_SESSION[ 'login' ][ 'email' ]		=	$email;
+	$_SESSION[ 'login' ][ 'password' ]	=	$pass;
+
+	if ( $isEmailValid && !empty( $pass )) {
+
+		//___make connection to database___//
+        $msqlConn = new MsqlConnect( 'CRUD_CMS', 'jolita', 'zN6br4fLYVJ8pSNy' );
+
+        $loggedIn = User::validateLogin( $msqlConn, $email, $pass );
+
+		if ( $loggedIn ) {
+
+			Message::setMessage('U bent ingelogd.', 'success'); 
+			Header('Location: dashboard.php');
+
+		}
+		else{
+
+		Message::setMessage('Er ging iets mis. Probeer opnieuw.', 'error');
+		Header('Location: login-form.php');
+		}
+	
+	}
+	else{
+
+		Message::setMessage('Dit is geen geldig e-mailadres. Vul een geldig e-mailadres in.', 'error');
+		Header('Location: login-form.php');
+
+	}
+
+
+	
+}
+
+		
+
+ ?>
