2ec47dcbdb42c0c03a429224f902bd3be1c8b0b4
Jolita Pabludo <jolita.grazyte@student.kdg.be>
2014-12-10 16:21:29 +0100

    mod_rewrite basis deli

---
+++ 034_mod-rewrite-basis/deel2/original.php
@@ -0,0 +1,20 @@
+
+ <!doctype html>
+ <html>
+     <head>
+         <meta charset="utf-8">
+         <meta name="description" content="">
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+         <title>Untitled</title>
+         <link rel="stylesheet" href="css/style.css">
+         <link rel="author" href="humans.txt">
+     </head>
+     <body>
+         <h1>ORIGINAL</h1>
+         <a href="rasmus">R A S M U S</a>
+         <?php 
+var_dump($_GET);
+
+ ?>
+     </body>
+ </html>
